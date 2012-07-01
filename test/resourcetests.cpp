00c8814298e228cf8a1d6567def9397769f1c84b
Vishesh Handa <me@vhanda.in>
2012-07-01 16:21:35 +0530

    Add tests for Nepomuk::Resource
    Created a list of tests that should be written for Nepomuk. Also
    implemented two of them. A lot of stuff already fails! We should have
    implemented this long ago.

---
+++ test/resourcetests.cpp
@@ -0,0 +1,151 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "resourcetests.h"
+
+#include <KDebug>
+#include <KTemporaryFile>
+#include <qtest_kde.h>
+
+#include <Soprano/Vocabulary/RDF>
+#include <Soprano/Vocabulary/NAO>
+#include <Soprano/Statement>
+#include <Soprano/Model>
+#include <Soprano/StatementIterator>
+#include <Nepomuk2/Vocabulary/NCO>
+
+#include <Nepomuk2/Resource>
+#include <Nepomuk2/Variant>
+#include <Nepomuk2/Tag>
+#include <Nepomuk2/ResourceManager>
+#include <Nepomuk2/DataManagement>
+
+using namespace Soprano::Vocabulary;
+using namespace Nepomuk2::Vocabulary;
+
+namespace Nepomuk2 {
+
+void ResourceTests::newTag()
+{
+    Resource tag("Test", NAO::Tag());
+    QVERIFY(!tag.exists());
+
+    //QCOMPARE(tag.label(), QString("Test"));
+    QVERIFY(!tag.exists());
+    QVERIFY(tag.resourceUri().isEmpty());
+
+    // Resources are only saved when some changes are made
+    // FIXME: Maybe add some save function?
+    tag.setLabel("Test");
+
+    QVERIFY(tag.exists());
+    QVERIFY(!tag.resourceUri().isEmpty());
+    QCOMPARE(tag.resourceType(), NAO::Tag());
+
+    // Check the statements
+    Soprano::Model* model = ResourceManager::instance()->mainModel();
+    QList<Soprano::Statement> stList = model->listStatements( tag.resourceUri(), Soprano::Node(),
+                                                              Soprano::Node() ).allStatements();
+
+    // One for <resUri> nao:lastModified ..
+    //         <resUri> nao:created ..
+    //         <resUri> rdf:type nao:Tag
+    //         <resUri> nao:identifier "Test"
+    //         <resUri> nao:prefLabel "Test" - plain literal
+    QCOMPARE(stList.size(), 5);
+
+    QVERIFY(model->containsAnyStatement(tag.resourceUri(), NAO::lastModified(), Soprano::Node()));
+    QVERIFY(model->containsAnyStatement(tag.resourceUri(), NAO::created(), Soprano::Node()));
+    QVERIFY(model->containsAnyStatement(tag.resourceUri(), RDF::type(), NAO::Tag()));
+    QVERIFY(model->containsAnyStatement(tag.resourceUri(), NAO::identifier(), Soprano::LiteralValue("Test")));
+    QVERIFY(model->containsAnyStatement(tag.resourceUri(), NAO::prefLabel(), Soprano::LiteralValue::createPlainLiteral("Test")));
+}
+
+void ResourceTests::newContact()
+{
+    Resource con( QUrl(), NCO::Contact() );
+    QVERIFY(!con.exists());
+    QVERIFY(con.properties().empty());
+    QVERIFY(con.resourceUri().isEmpty());
+
+    QString martin("Martin Klapetek");
+    con.setProperty(NCO::fullname(), martin);
+    QVERIFY(!con.resourceType().isEmpty());
+    QCOMPARE(con.property(NCO::fullname()).toString(), martin);
+
+    // Cross check the statements
+    //
+    Soprano::Model* model = ResourceManager::instance()->mainModel();
+    QList<Soprano::Statement> stList = model->listStatements( con.resourceUri(), Soprano::Node(),
+                                                              Soprano::Node() ).allStatements();
+    // One for <resUri> nao:lastModified ..
+    //         <resUri> nao:created ..
+    //         <resUri> rdf:type nco:Contact
+    //         <resUri> nco:fullname "Martin Klapetek"
+    kDebug() << stList;
+    QCOMPARE(stList.size(), 4);
+
+    const QUrl uri = con.resourceUri();
+    QVERIFY(model->containsAnyStatement(uri, NAO::lastModified(), Soprano::Node()));
+    QVERIFY(model->containsAnyStatement(uri, NAO::created(), Soprano::Node()));
+    QVERIFY(model->containsAnyStatement(uri, RDF::type(), NCO::Contact()));
+    QVERIFY(model->containsAnyStatement(uri, NCO::fullname(), Soprano::LiteralValue(martin)));
+}
+
+void ResourceTests::newFile()
+{
+    /*KTemporaryFile tempFile;
+    QVERIFY(tempFile.open());
+
+    Resource file(*/
+}
+
+void ResourceTests::existingFile()
+{
+
+}
+
+void ResourceTests::existingContact()
+{
+
+}
+
+void ResourceTests::existingTag()
+{
+
+}
+
+void ResourceTests::initTagChange()
+{
+
+}
+
+void ResourceTests::initUrlChange()
+{
+
+}
+
+void ResourceTests::checkRating()
+{
+
+}
+
+}
+
+QTEST_KDEMAIN(Nepomuk2::ResourceTests, NoGUI)
---
+++ test/resourcetests.h
@@ -0,0 +1,78 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef RESOURCETESTS_H
+#define RESOURCETESTS_H
+
+#include <QtCore/QObject>
+#include <Nepomuk2/testbase.h>
+
+namespace Nepomuk2 {
+
+class ResourceTests : public Nepomuk2::TestBase
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    // Required Tests -
+    // 1. New Resources -
+    //    a. Make sure you can create tags
+    //    b. Make sure you can create stuff with custom types
+    //    c. Make sure you can create files
+
+    void newTag();
+    void newContact();
+    void newFile();
+
+    // 2. Existing resources
+    //    a. Can identify existing tags
+    //    b. Can identify existing nepomuk uris
+    //    c. Can identify files
+
+    void existingTag();
+    void existingContact();
+    void existingFile();
+
+    // 3. Properties
+    //    a. Make sure you can set/add/remove properties
+
+    void checkRating();
+
+    // 4. Initialization
+    //    a. Init via tag - Change tag name, and see if the old tag exists
+    //    b. Init via url - Change url, and same check
+
+    void initTagChange();
+    void initUrlChange();
+
+    // 5. Type checks -
+    //    a. Make sure the top most type is being given
+    //    b. Make sure pimo type is returned when present
+    //    c. Make sure tags have been given the correct type
+    //    d. Make sure files and folders have the correct type
+    // 6. Resource Watcher
+    //    a. Make sure tags are updated - Also cross check initialization
+    //    b. Check if the meta properties are updated
+    //    c. Make sure direct properties like ratings
+    // 7. Resource Deletion
+    //
+};
+
+}
+#endif // RESOURCETESTS_H
