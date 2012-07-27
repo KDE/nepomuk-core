ed7541bc719de532017e13981a05ea805698bd83
Vishesh Handa <me@vhanda.in>
2012-07-27 17:53:04 +0530

    TestLib: Cleanup the custon dbus session code
    dbus-launch was called with --exit-with-session, which creates a
    persistent "babysitter" process that watches stdin for SIGHUP, and tries
    to connect to the X-Server. If this process gets a SIGHUP or looses its
    conection to the X-server, it kills the message bus-daemon.
    
    We do not require any of this overhead, cause we do not actually run a
    separate x-server for the tests.
    
    The Dbus code is now split into two files -
    * dbus-session-begin.sh
    * dbus-session-end.sh - Manually kills the DBUS_SESSION_BUS_PID
    
    The scripts are called in nepomuk-sandbox-{begin,end}.sh

--- autotests/lib/tools/dbus-session-begin.sh
+++ autotests/lib/tools/dbus-session-begin.sh
@@ -2,7 +2,4 @@
 
 # start the new dbus session bus and export the env variables it creates
 echo "Starting new DBus session..."
-eval `dbus-launch --sh-syntax --exit-with-session`
-
-# start the process that is to be run in this new session
-# eval $@
+eval `dbus-launch --sh-syntax`
