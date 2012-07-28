087f16924147c8f6e738c691d3ce638bd5d01b4d
Patrick Spendrin <ps_ml@gmx.de>
2012-07-28 14:46:44 +0200

    add declaration of createResource needed for msvc

--- libnepomukcore/datamanagement/createresourcejob.cpp
+++ libnepomukcore/datamanagement/createresourcejob.cpp
@@ -19,6 +19,8 @@
    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+
+#include "datamanagement.h"
 #include "createresourcejob.h"
 #include "datamanagementinterface.h"
 #include "dbustypes.h"
