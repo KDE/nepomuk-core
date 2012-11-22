KDE/nepomuk-core

commit b54cbaa5e00c1191cd2e66399aeb86a04ba85ef1

DataMangaement: Optimize createUri
Check if the uri exists by using virtuoso's internal iri_to_id, instead
of a complicated sparql query that takes a long time to execute.

We can save save roughly 100-200 msecs by not running the huge query.

REVIEW: 106483

services/storage/datamanagementmodel.cpp
@@ -2446,18 +2446,18 @@ QUrl Nepomuk2::DataManagementModel::createUri(Nepomuk2::DataManagementModel::Uri
     while( 1 ) {
         QString uuid = QUuid::createUuid().toString();
         uuid = uuid.mid(1, uuid.length()-2);
-        const QUrl uri = QUrl( QLatin1String("nepomuk:/") + typeToken + QLatin1String("/") + uuid );
-        if ( !FilterModel::executeQuery( QString::fromLatin1("ask where { "
-                                                             "{ %1 ?p1 ?o1 . } "
-                                                             "UNION "
-                                                             "{ ?s2 %1 ?o2 . } "
-                                                             "UNION "
-                                                             "{ ?s3 ?p3 %1 . } "
-                                                             "UNION "
-                                                             "{ graph %1 { ?s4 ?4 ?o4 . } . } "
-                                                             "}")
-                                        .arg( Soprano::Node::resourceToN3(uri) ), Soprano::Query::QueryLanguageSparql ).boolValue() ) {
-            return uri;
+
+        QString uriString = QString::fromLatin1("nepomuk:/%1/%2").arg( typeToken, uuid );
+        const QUrl uri( uriString );
+
+        // The iri_to_id command returns an id of the form "#num" if the uri exists in the database
+        // and NULL if it doesn't. This method is a LOT faster than checking if the uri exists
+        // as a subject / predicate / object or graph.
+        QString query = QString::fromLatin1("select iri_to_id( '%1', 0 )") .arg( uriString );
+        Soprano::QueryResultIterator it = executeQuery( query, Soprano::Query::QueryLanguageUser, QLatin1String("sql") );
+        if( it.next() ) {
+            if( it[0].literal().toString().isEmpty() )
+                return uri;
         }
     }
 }

commit 7d0213d9ca3c93b3909d1cf3ef5dec874a97bb7c

Do not loop forever if there is an error executing the query in createUri (for example, the connection to the server died). Instead just return an empty url.
Also check for such an error with createGraph in ResourceMerger::merge.

This fixes a bug sort of introduced by b54cbaa5e00c1191cd2e66399aeb86a04ba85ef1.
If something went wrong with virtuoso, xsession-errors would fill up the
available space, because each loop called executeQuery and added an error message to the log.

REVIEW: 107318

services/storage/datamanagementmodel.cpp
@@ -2466,6 +2466,10 @@ QUrl Nepomuk2::DataManagementModel::createUri(Nepomuk2::DataManagementModel::Uri
         // as a subject / predicate / object or graph.
         QString query = QString::fromLatin1("select iri_to_id( '%1', 0 )") .arg( uriString );
         Soprano::QueryResultIterator it = executeQuery( query, Soprano::Query::QueryLanguageUser, QLatin1String("sql") );
+        //Don't loop forever if there was an error executing the query.
+        if( lastError() ){
+            return QUrl();
+        }
         if( it.next() ) {
             if( it[0].literal().toString().isEmpty() )
                 return uri;

services/storage/resourcemerger.cpp
@@ -668,6 +668,8 @@ bool Nepomuk2::ResourceMerger::merge(const Nepomuk2::Sync::ResourceHash& resHash
     // Create the main graph, if they are any statements to merge
     if( !resHash.isEmpty() ) {
         m_graph = m_model->createGraph( m_app, m_additionalMetadata );
+        if( m_graph.isEmpty() )
+            return false;
     }
 
 