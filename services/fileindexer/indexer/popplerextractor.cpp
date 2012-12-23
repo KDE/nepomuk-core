21fe48f2e216fb22b853fa28e68eb89b3b04eb7f
Jörg Ehrichs <Joerg.Ehrichs@gmx.de>
2012-12-23 12:25:10 +0100

    Fix title extraction for the popplerextraction
    Extend popplerextractor with firstpage parsing
    
    Often the pdf metadata is not available or wrong data is added
    to the title field (pdf exporter names instead of title).
    
    This patch adds the possibility to parse the first page for a possible
    title. A possibel title is determined by the connected text with the
    biggest font that was more than one character.
    
    REVIEW: 107870

--- services/fileindexer/indexer/popplerextractor.cpp
+++ services/fileindexer/indexer/popplerextractor.cpp
@@ -1,6 +1,7 @@
 /*
     <one line to give the library's name and an idea of what it does.>
     Copyright (C) 2012  Vishesh Handa <me@vhanda.in>
+    Copyright (C) 2012  Jörg Ehrichs <joerg.ehrichs@gmx.de>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -59,7 +60,19 @@ SimpleResourceGraph PopplerExtractor::extract(const QUrl& resUri, const QUrl& fi
         return graph;
     }
 
-    QString title = pdfDoc->info(QLatin1String("Title"));
+    QString title = pdfDoc->info(QLatin1String("Title")).trimmed();
+
+    // The title extracted from the pdf metadata is in many cases not the real title
+    // of the document. Especially for research papers that are exported to pdf.
+    // As mostly the title of a pdf document is written on the first page in the biggest font
+    // we use this if the pdfDoc title is considered junk
+    if(title.isEmpty() ||
+       !title.contains(' ') ||                        // very unlikely the title of a document does only contain one word.
+       title.contains(QLatin1String("Microsoft"), Qt::CaseInsensitive)) {  // most research papers i found written with microsoft word
+                                                                           // have a garbage title of the pdf creator rather than the real document title
+        title = parseFirstPage(pdfDoc);
+    }
+
     if( !title.isEmpty() ) {
         fileRes.addProperty( NIE::title(), title );
     }
@@ -96,6 +109,77 @@ SimpleResourceGraph PopplerExtractor::extract(const QUrl& resUri, const QUrl& fi
 
     graph << fileRes;
     return graph;
+}
+
+QString PopplerExtractor::parseFirstPage(Poppler::Document* pdfDoc)
+{
+    Poppler::Page *p = pdfDoc->page(0);
+
+    QList<Poppler::TextBox*> tbList = p->textList();
+    QMap<int, QString> possibleTitleMap;
+
+    int currentLargestChar = 0;
+    int skipTextboxes = 0;
+
+    // Iterate over all textboxes. Each textbox can be a single character/word or textblock
+    // Here we combine the etxtboxes back together based on the textsize
+    // Important are the words with the biggest font size
+    foreach (Poppler::TextBox * tb, tbList) {
+
+        // if we added followup words, skip the textboxes here now
+        if (skipTextboxes > 0) {
+            skipTextboxes--;
+            continue;
+        }
+
+        int height = tb->charBoundingBox(0).height();
+
+        // if the following text is smaller than the biggest we found up to now, ignore it
+        if (height >= currentLargestChar) {
+            QString possibleTitle;
+            possibleTitle.append(tb->text());
+            currentLargestChar = height;
+
+            // if the text has follow up words add them to to create the full title
+            Poppler::TextBox * next = tb->nextWord();
+            while (next) {
+                possibleTitle.append(QLatin1String(" "));
+                possibleTitle.append(next->text());
+                next = next->nextWord();
+                skipTextboxes++;
+            }
+
+            // now combine text for each font size together, very likeley it must be connected
+            QString existingTitlePart = possibleTitleMap.value(currentLargestChar, QString());
+            existingTitlePart.append(QLatin1String(" "));
+            existingTitlePart.append(possibleTitle);
+            possibleTitleMap.insert(currentLargestChar, existingTitlePart);
+        }
+    }
+
+    qDeleteAll(tbList);
+    delete p;
+
+    QList<int> titleSizes = possibleTitleMap.keys();
+    qSort(titleSizes.begin(), titleSizes.end(), qGreater<int>());
+
+    QString newPossibleTitle;
+
+    // find the text with the largest font that is not just 1 character
+    foreach (int i, titleSizes) {
+        QString title = possibleTitleMap.value(i);
+
+        // sometime the biggest part is a single letter
+        // as a starting paragraph letter
+        if (title.size() < 5) {
+            continue;
+        } else {
+            newPossibleTitle = title.trimmed();
+            break;
+        }
+    }
+
+    return newPossibleTitle;
 }
 
 }
