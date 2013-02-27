37e6858179a0ebecf56d91f0542ae8de850a7b8f
Vishesh Handa <me@vhanda.in>
2013-02-28 03:54:55 +0530

    New Tool - nepomukshow
    It can used to dump the contents of any resource to stdout. It has
    additional flags which can be used to control if inferencing should be
    enabled or not.
    
    This is being imported from kde:scratch/vhanda/nepomuk-show. I don't
    care that much about the history that much, so I'm not importing it.

---
+++ tools/nepomukshow/main.cpp
@@ -0,0 +1,140 @@
+/*
+   Copyright (c) 2010 Sebastian Trueg <trueg@kde.org>
+   Copyright (c) 2012 Vishesh Handa <me@vhanda.in>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of
+   the License or (at your option) version 3 or any later version
+   accepted by the membership of KDE e.V. (or its successor approved
+   by the membership of KDE e.V.), which shall act as a proxy
+   defined in Section 14 of version 3 of the license.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <QtCore/QCoreApplication>
+#include <QtCore/QHash>
+#include <QtCore/QUrl>
+#include <QtCore/QSet>
+
+#include <kcmdlineargs.h>
+#include <kaboutdata.h>
+#include <KDE/KLocale>
+#include <KComponentData>
+#include <KUrl>
+#include <KDebug>
+
+#include <Soprano/Model>
+#include <Soprano/QueryResultIterator>
+
+#include "resourceprinter.h"
+#include "resource.h"
+#include "variant.h"
+#include "property.h"
+#include "resourcemanager.h"
+
+int main( int argc, char *argv[] )
+{
+    KAboutData aboutData( "nepomukshow",
+                          "nepomukshow",
+                          ki18n("Nepomuk Show"),
+                          "0.1",
+                          ki18n("The Nepomuk Resource Viewer - A debugging tool"),
+                          KAboutData::License_GPL,
+                          ki18n("(c) 2012, Vishesh Handa"),
+                          KLocalizedString(),
+                          "http://nepomuk.kde.org" );
+    aboutData.addAuthor(ki18n("Vishesh Handa"),ki18n("Maintainer"), "me@vhanda.in");
+    aboutData.addAuthor(ki18n("Sebastian Trüg"),ki18n("Developer"), "trueg@kde.org");
+    aboutData.setProgramIconName( "nepomuk" );
+
+    KCmdLineArgs::init( argc, argv, &aboutData );
+
+    KCmdLineOptions options;
+	options.add("+resource", ki18n("The resource URI, a file URL, or a file path."));
+    options.add("depth <depth>", ki18n("The number of sub resources to list"), "0");
+    options.add("graphs", ki18n("Print information about the graphs"));
+    options.add("inference", ki18n("Uses Inference to print additional properties"));
+    KCmdLineArgs::addCmdLineOptions( options );
+
+    KCmdLineArgs* args = KCmdLineArgs::parsedArgs();
+
+    QCoreApplication app( argc, argv );
+    KComponentData comp( aboutData );
+
+    QTextStream err( stdout );
+
+    //
+    // The Resource Uri
+    //
+    const QUrl url = args->url( 0 );
+    Nepomuk2::Resource res( url );
+    if ( !res.exists() ) {
+        err << "Resource does not exist: " << args->url( 0 ).url() << endl;
+        return 1;
+    }
+
+    Soprano::Model* model = Nepomuk2::ResourceManager::instance()->mainModel();
+    QSet<QUrl> uriList;
+
+    //
+    // If nie:url, then fetch the resources
+    //
+    if( url.scheme() != QLatin1String("nepomuk") ) {
+        QString query = QString::fromLatin1("select ?r where { ?r nie:url %1 . }")
+                        .arg( Soprano::Node::resourceToN3( url ) );
+        Soprano::QueryResultIterator it = model->executeQuery( query, Soprano::Query::QueryLanguageSparql );
+        while( it.next() ) {
+            uriList << it[0].uri();
+        }
+    }
+    else {
+        uriList << res.uri();
+    }
+
+    //
+    // Fetch additional resource for the depth
+    //
+    int depth = args->getOption("depth").toInt();
+    int oldSize = uriList.size();
+    for( int i=0; i<depth; i++ ) {
+        QSet<QUrl> newUris;
+        foreach(const QUrl& uri, uriList) {
+            QString query = QString::fromLatin1("select ?o where { %1 ?p ?o. FILTER(REGEX(STR(?o), '^nepomuk')). }")
+                            .arg( Soprano::Node::resourceToN3(uri) );
+            Soprano::QueryResultIterator it = model->executeQuery( query, Soprano::Query::QueryLanguageSparql );
+            while( it.next() )
+                newUris << it[0].uri();
+        }
+        uriList.unite( newUris );
+
+        if( uriList.size() == oldSize )
+            break;
+        else
+            oldSize = uriList.size();
+    }
+
+    //
+    // Dump to stdout
+    //
+    QTextStream stream( stdout );
+    Nepomuk2::ResourcePrinter printer;
+
+    if( args->isSet("graphs") )
+        printer.setGraphs( true );
+    if( args->isSet("inference") )
+        printer.setInference( true );
+
+    foreach( const QUrl& resUri, uriList ) {
+        printer.print( stream, resUri );
+    }
+
+    return 0;
+}
---
+++ tools/nepomukshow/resourceprinter.cpp
@@ -0,0 +1,238 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright (C) 2012  Vishesh Handa <me@vhanda.in>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "resourceprinter.h"
+#include "uri.h"
+
+#include <QtCore/QSet>
+#include <QDateTime>
+
+#include <Soprano/Model>
+#include <Soprano/QueryResultIterator>
+#include <Soprano/Vocabulary/NAO>
+#include <Soprano/Vocabulary/RDF>
+#include <Soprano/Vocabulary/NRL>
+
+#include "resourcemanager.h"
+#include "nie.h"
+
+using namespace Soprano::Vocabulary;
+using namespace Nepomuk2::Vocabulary;
+
+namespace {
+
+    struct SyncResource {
+        QUrl uri;
+        QMultiHash<QUrl, Soprano::Node> prop;
+    };
+
+    SyncResource createSyncResource(const QUrl& uri, bool inference = false) {
+        SyncResource syncRes;
+        syncRes.uri = uri;
+
+        QString query = QString::fromLatin1("select ?p ?o where { %1 ?p ?o. }")
+                        .arg( Soprano::Node::resourceToN3( uri ) );
+
+        Soprano::Query::QueryLanguage lang;
+        if( inference )
+            lang = Soprano::Query::QueryLanguageSparql;
+        else
+            lang = Soprano::Query::QueryLanguageSparqlNoInference;
+
+        Soprano::Model* model = Nepomuk2::ResourceManager::instance()->mainModel();
+        Soprano::QueryResultIterator it = model->executeQuery( query, lang );
+        while( it.next() ) {
+            Soprano::Node object = it[1];
+
+            // Do not show the nie:plainTextContent. It's too large.
+            if( it[0].uri() != NIE::plainTextContent() )
+                syncRes.prop.insertMulti( it[0].uri(), object );
+        }
+
+        return syncRes;
+    }
+
+    class TablePrinter {
+    public:
+        TablePrinter( int columns ) {
+            m_columns = columns;
+
+            // Isn't there a better way?
+            for( int i = 0; i<columns; i++ ) {
+                m_data.append( QStringList() );
+                m_dataMaxLength.append( 0 );
+            }
+        }
+
+        void insert( int column, const QString& data ) {
+            m_data[ column ] << data;
+            if( data.length() > m_dataMaxLength[column] )
+                m_dataMaxLength[column] = data.length();
+        }
+
+        void print( QTextStream& stream ) {
+            int rows = m_data[0].size();
+
+            for( int i=0; i<rows; i++ ) {
+                // For the initial indentation - Make it configurable
+                stream << QString( 8, QChar::fromAscii(' ') );
+                for( int c=0; c<m_columns; c++ ) {
+                    stream << m_data[c][i];
+
+                    int numSpaces = m_dataMaxLength[c] - m_data[c][i].length();
+                    numSpaces += 2;
+
+                    stream << QString( numSpaces, QChar::fromAscii(' ') );
+                }
+                stream << "\n";
+            }
+        }
+
+    private:
+        int m_columns;
+
+        QList<QStringList> m_data;
+        QList<int> m_dataMaxLength;
+    };
+
+    QList<QUrl> sortProperties(const QList<QUrl>& properties) {
+        // Group the keys based on prefix
+        QMultiHash<QString, QUrl> groupedProperties;
+        foreach( const QUrl& propUri, properties ) {
+            groupedProperties.insert( Nepomuk2::Uri(propUri).prefix(), propUri );
+        }
+
+        // Print on the basis of a custom sort
+        QList<QUrl> sortedKeys;
+
+        // Custom Sort
+        sortedKeys += groupedProperties.values("rdf");
+        sortedKeys += groupedProperties.values("rdfs");
+        sortedKeys += groupedProperties.values("nrl");
+        sortedKeys += groupedProperties.values("nao");
+        sortedKeys += groupedProperties.values("nie");
+        sortedKeys += groupedProperties.values("nfo");
+        sortedKeys += groupedProperties.values("nmm");
+        sortedKeys += groupedProperties.values("kext");
+        sortedKeys += properties.toSet().subtract( sortedKeys.toSet() ).toList(); // Remaining
+
+        return sortedKeys;
+    }
+
+
+    QString nodeToN3(const Soprano::Node& node) {
+        QString str;
+        if( node.isResource() ) {
+            if( node.uri().scheme() == QLatin1String("nepomuk") || node.uri().scheme() == QLatin1String("file") )
+                str = node.uri().toString();
+            else
+                str = Nepomuk2::Uri( node.uri() ).toN3();
+        }
+        else {
+            str = node.literal().toString();
+        }
+        return str;
+    }
+
+    QString printGraphData(const QUrl& graphUri) {
+        SyncResource res = createSyncResource(graphUri);
+
+        QString app;
+        QDateTime created;
+        bool discardable = false;
+
+        if( res.prop.contains(NAO::maintainedBy()) ) {
+            QString query = QString::fromLatin1("select ?i where { %1 nao:identifier ?i. }")
+                            .arg( res.prop.value(NAO::maintainedBy()).toN3() );
+            Soprano::Model* model = Nepomuk2::ResourceManager::instance()->mainModel();
+            Soprano::QueryResultIterator it = model->executeQuery( query, Soprano::Query::QueryLanguageSparql );
+            if( it.next() ) {
+                app = it[0].literal().toString();
+            }
+        }
+
+        if( res.prop.contains(NAO::created()) ) {
+            created = res.prop.value(NAO::created()).literal().toDateTime();
+        }
+
+        if( res.prop.contains(RDF::type(), Soprano::Node(NRL::DiscardableInstanceBase())) ) {
+            discardable = true;
+        }
+
+        QString str = "| " + app + " | " + created.toLocalTime().toString( Qt::ISODate );
+        if( discardable )
+            str += " | D";
+
+        return str;
+    }
+
+    void print(QTextStream& stream, const SyncResource& syncRes, bool printGraphInfo) {
+        // Print the uri
+        stream << "\n" << Soprano::Node::resourceToN3( syncRes.uri ) << "\n";
+
+        TablePrinter tablePrinter( printGraphInfo ? 3: 2 );
+
+        // Print on the basis of a custom sort
+        QList<QUrl> sortedKeys = sortProperties( syncRes.prop.uniqueKeys() );
+        foreach( const QUrl& propUri, sortedKeys ) {
+            QList<Soprano::Node> values = syncRes.prop.values( propUri );
+            foreach(const Soprano::Node& node, values) {
+
+                QString propery = Nepomuk2::Uri( propUri ).toN3();
+                QString object = nodeToN3( node );
+
+                tablePrinter.insert( 0, propery );
+                tablePrinter.insert( 1, object );
+
+                if( printGraphInfo ) {
+                    Soprano::Model* model = Nepomuk2::ResourceManager::instance()->mainModel();
+                    QString query = QString::fromLatin1("select ?g where { graph ?g { %1 %2 %3. } }")
+                                    .arg( Soprano::Node::resourceToN3(syncRes.uri),
+                                          Soprano::Node::resourceToN3(propUri),
+                                          node.toN3() );
+                    Soprano::QueryResultIterator it = model->executeQuery( query, Soprano::Query::QueryLanguageSparql );
+                    if( it.next() )
+                        tablePrinter.insert( 2, printGraphData( it[0].uri() ) );
+                    else
+                        tablePrinter.insert( 2, "---");
+                }
+            }
+        }
+
+        tablePrinter.print( stream );
+    }
+
+}
+
+
+void Nepomuk2::ResourcePrinter::print(QTextStream& stream, const QUrl& uri)
+{
+    ::print( stream, createSyncResource(uri, m_inference), m_printGraphs );
+}
+
+void Nepomuk2::ResourcePrinter::setGraphs(bool status)
+{
+    m_printGraphs = status;
+}
+
+void Nepomuk2::ResourcePrinter::setInference(bool status)
+{
+    m_inference = status;
+}
