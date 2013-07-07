e2dcb87300fe7f7cc58ca3270c34b2a8a9d2a824
Vishesh Handa <me@vhanda.in>
2013-07-08 02:23:31 +0530

    Revert "Utils::formatPropertyValue: Avoid connecting to Nepomuk if it is not running"
    This reverts commit 21cac12b13b6967ae3a1911ee2ceb52d7397d744.
    
    This was done as a minor optimization, and it breaks the code in certain
    cases. A proper optimization should be done inside the Type classes.
    
    BUG: 321788

--- libnepomukcore/misc/utils.cpp
+++ libnepomukcore/misc/utils.cpp
@@ -136,7 +136,7 @@ QString Nepomuk2::Utils::formatPropertyValue( const Nepomuk2::Types::Property& p
         valueString = KGlobal::locale()->formatTime( time, true, true );
     }
 
-    if( valueString.isEmpty() && ResourceManager::instance()->initialized() ) {
+    if( valueString.isEmpty() ) {
         Types::Literal literalRange = property.literalRangeType();
         QVariant::Type dataType = literalRange.dataType();
         if( !literalRange.isValid() )
